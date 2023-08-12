b19ffe48418921b5969a140348ca6e14dde442ed
Sirius-13 <cwxdesmond2002@gmail.com>
2023-08-12 17:45:52 +0800

    1. added scroll to top button 2. adjusted several page

--- javascript/home.js
+++ javascript/home.js
@@ -9,3 +9,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
+
+function scrollFunction() {
+    let mybutton = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+window.onscroll = function () {
+    scrollFunction();
+};
